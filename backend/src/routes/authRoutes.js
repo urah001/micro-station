5b89399b7b43befe2f1ca25b4341cca4b73b41a6
STEPHEN SAMUEL URAH <stephensamuel063@gmail.com>
2025-10-06 18:02:58 +0100

    added the auth controller and auth route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,50 +1,9 @@
 import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import prisma from "../prismaClient.js";
-import dotenv from "dotenv";
+import { loginUser, registerUser } from "../controllers/authController.js";
 
-dotenv.config();
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password)
-    return res.status(400).json({ message: "All fields required" });
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await prisma.user.create({
-      data: { name, email, password: hashedPassword },
-    });
-
-    res.json({ message: "Registration successful", user });
-  } catch (error) {
-    res.status(500).json({ message: "Error registering user", error });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-      return res.status(401).json({ message: "Invalid credentials" });
-
-    const token = jwt.sign(
-      { id: user.id, email: user.email, isAdmin: user.isAdmin },
-      process.env.JWT_SECRET,
-      { expiresIn: "7d" }
-    );
-
-    res.json({ token, user });
-  } catch (error) {
-    res.status(500).json({ message: "Login error", error });
-  }
-});
+router.post("/login", loginUser);
+router.post("/register", registerUser);
 
 export default router;
